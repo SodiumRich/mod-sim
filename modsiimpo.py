4bde1289846fb6873b46e7a051f0138d8281cef4
SodiumRich <98496933+SodiumRich@users.noreply.github.com>
2022-01-27 17:39:10 -0500

    Update modsiimpo.py

--- modsiimpo.py
+++ modsiimpo.py
@@ -9,26 +9,31 @@ import numpy as np
 import random
 
 #Define number of runs
-runs = 1
-applicants = []
+runs = 10000
 yescounter = np.zeros((runs,30))
 
 
 for i in range(runs):
     applicants = random.sample(range(0,30),30) #Fills applicants with a random sample with no replicates ranging from 1 to 30
-   
+    
     for m in range(30):
-        if m == 0: #For m of 0 the yes counter increases if and only if the first applicant is the best applicant
+        
+        if m == 0:
             if applicants[0] == 0:
-                yescounter[i,m] = 1
+                yescounter[i][m] = 1
+            else:
+                yescounter[i][m] = 0
+        
         else:
             best_look = min(applicants[:m])
+            
             for k in applicants[m:]:
-                print(k)
-                if applicants[k] < best_look:
-                    yescounter[i,k] = 1
-                    break
-                    
+                if k < best_look:
+                    if k == 0:
+                        yescounter[i][m] = 1
+                    else:
+                       yescounter[i][m] = 0
+                
 
 yescountersum = yescounter.sum(axis=0)
 
